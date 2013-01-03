6c6940677ad9e75747adf7df41ec7f8861e607b9
Nathan Koch <nathanrkoch@gmail.com>
2013-01-03 15:33:57 -0600

    chapter 2 example 5

---
+++ chapter02/Example5/src/Example5App.cpp
@@ -0,0 +1,74 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Rand.h"
+#include "Mover.h"
+#include "Liquid.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class Example5App : public AppBasic {
+  public:
+	void setup();
+	void update();
+	void draw();
+    void prepareSettings(Settings *settings);
+    
+    vector<Mover*> mMovers;
+    int mNumMovers;
+    
+    Liquid *liquid;
+};
+
+void Example5App::prepareSettings(Settings *settings) {
+    settings->setFullScreen();
+}
+
+void Example5App::setup()
+{
+    mNumMovers = 20;
+    int i = 0;
+    while (i < mNumMovers) {
+        mMovers.push_back(new Mover(randFloat(0.1, 5.0), randFloat(getWindowWidth()), randFloat(getWindowHeight())));
+        i++;
+    }
+    liquid = new Liquid();
+}
+
+void Example5App::update()
+{
+    Vec2f wind = Vec2f(0.05, 0);
+    for (auto mover : mMovers) {
+        if (liquid->contains(mover->mLocation)) {
+            mover->drag(liquid);
+        }
+        
+        mover->applyForce(wind);
+        
+        float c = 0.01;
+        Vec2f friction = mover->mVelocity;
+        friction *= -1;
+        friction.safeNormalize();
+        friction *= c;
+        mover->applyForce(friction);
+
+        float m = (0.1 * mover->mMass);
+        Vec2f gravity = Vec2f(0, m);
+        mover->applyForce(gravity);
+
+        mover->update();
+        mover->checkEdges();
+    }
+}
+
+void Example5App::draw()
+{
+	gl::clear( Color( 1, 1, 1 ) );
+    liquid->draw();
+    for (auto mover : mMovers) {
+        mover->draw();
+    }
+}
+
+CINDER_APP_BASIC( Example5App, RendererGl )
---
+++ chapter02/Example5/xcode/Mover.h
@@ -0,0 +1,32 @@
+/*
+ *  Mover.h
+ *  Example1
+ *
+ *  Created by Nathan Koch on 1/3/13
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#pragma once
+#include "Liquid.h"
+
+class Mover {
+	
+  public:
+	
+	Mover();
+    Mover(float m, float x, float y);
+	
+	void update();
+	void draw();
+    void applyForce(ci::Vec2f force);
+    void checkEdges();
+    void drag(Liquid *liquid);
+    
+    ci::Vec2f mLocation;
+    ci::Vec2f mVelocity;
+    ci::Vec2f mAcceleration;
+    
+    float mMass;
+	
+};
