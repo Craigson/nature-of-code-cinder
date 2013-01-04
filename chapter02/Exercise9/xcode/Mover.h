4e76dff6fb133a7d2fbd5ebe6e1e58a9fdfd917f
Nathan Koch <nathanrkoch@gmail.com>
2013-01-04 15:07:49 -0600

    chapter 2 exercise 9

---
+++ chapter02/Exercise9/xcode/Mover.h
@@ -0,0 +1,31 @@
+/*
+ *  Mover.h
+ *  Example1
+ *
+ *  Created by Nathan Koch on 1/3/13
+ *  Copyright 2013 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#pragma once
+
+typedef std::shared_ptr<class Mover> MoverRef;
+class Mover {
+	
+  public:
+	
+	Mover();
+    Mover(float m, float x, float y);
+	
+	void update();
+	void draw();
+    void applyForce(ci::Vec2f force);
+    void checkEdges();
+    
+    ci::Vec2f mLocation;
+    ci::Vec2f mVelocity;
+    ci::Vec2f mAcceleration;
+    
+    float mMass;
+	
+};
